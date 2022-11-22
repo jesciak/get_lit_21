319d7253e669a0bdb87d3d00bf41a2304c5d6204
jesciak <jesciak@yahoo.com>
2022-11-21 23:32:14 -0500

    set up Client App.js file importing apollo and constructing GraphQL

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,34 @@
 import React from 'react';
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  createHttpLink,
+} from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import SearchBooks from './pages/SearchBooks';
 import SavedBooks from './pages/SavedBooks';
 import Navbar from './components/Navbar';
+
+// Construct our main GraphQL API endpoint
+const httpLink = createHttpLink({
+  uri: '/graphql',
+});
+
+const authLink = setContext((_, { headers }) => {
+  const toke = localStorage.getItem('id_token');
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
 
 function App() {
   return (
@@ -10,15 +36,15 @@ function App() {
       <>
         <Navbar />
         <Routes>
-          <Route 
-            path='/' 
-            element={<SearchBooks />} 
+          <Route
+            path='/'
+            element={<SearchBooks />}
           />
-          <Route 
-            path='/saved' 
-            element={<SavedBooks />} 
+          <Route
+            path='/saved'
+            element={<SavedBooks />}
           />
-          <Route 
+          <Route
             path='*'
             element={<h1 className='display-2'>Wrong page!</h1>}
           />
